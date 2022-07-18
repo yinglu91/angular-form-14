7e1296912bb83b412d22edbef3940972d9fe2476
Ying Lu <ylu@ert.com>
2022-07-17 23:01:22 -0400

    new course step 2

--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -1,19 +1,45 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { createPromoRangeValidator } from "../../validators/date-range.validator";
 
 @Component({
-  selector: 'create-course-step-2',
-  templateUrl: 'create-course-step-2.component.html',
-  styleUrls: ['create-course-step-2.component.scss']
+  selector: "create-course-step-2",
+  templateUrl: "create-course-step-2.component.html",
+  styleUrls: ["create-course-step-2.component.scss"],
 })
 export class CreateCourseStep2Component implements OnInit {
+  form = this.fb.group(
+    {
+      courseType: ["premium", Validators.required],
+      price: [
+        null,
+        [
+          Validators.required,
+          Validators.min(1),
+          Validators.max(9999),
+          Validators.pattern("[0-9]+"),
+        ],
+      ],
+      promoStartAt: [null],
+      promoEndAt: [null],
+    },
+    {
+      validators: [createPromoRangeValidator()],
+      updateOn: "blur",
+    }
+  );
 
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.form.valueChanges.subscribe((val) => {
+      const priceControl = this.form.controls["price"];
 
-
-
+      if (val.courseType === "free" && priceControl.enabled) {
+        priceControl.disable({ emitEvent: false });
+      } else if (val.courseType === "premium" && priceControl.disabled) {
+        priceControl.enable({ emitEvent: false });
+      }
+    });
   }
-
 }
--- src/app/create-course/create-course.component.ts
+++ src/app/create-course/create-course.component.ts
@@ -1,19 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
-
-
-
+import { Component, OnInit } from "@angular/core";
+import { STEPPER_GLOBAL_OPTIONS } from "@angular/cdk/stepper";
 
 @Component({
-  selector: 'create-course',
-  templateUrl: './create-course.component.html',
-  styleUrls: ['./create-course.component.scss']
+  selector: "create-course",
+  templateUrl: "./create-course.component.html",
+  styleUrls: ["./create-course.component.scss"],
+  providers: [
+    { provide: STEPPER_GLOBAL_OPTIONS, useValue: { showError: true } },
+  ],
 })
 export class CreateCourseComponent implements OnInit {
-
-
-  ngOnInit() {
-
-  }
-
+  ngOnInit() {}
 }
